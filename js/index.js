a7c2589e3b0a7fb92ce1d79130969a78b63d3a2d
unknown <pedroquispe030418@gmail.com>
2021-03-02 09:52:19 -0500

    Cuarto Commit Ventana Emergente Popup

--- js/index.js
+++ js/index.js
@@ -14,5 +14,20 @@ closeNav.addEventListener("click", () => {
     }
 });
 
+
+// PopUp
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
+
+closePopup.addEventListener("click", () => {
+  popup.classList.remove("show");
+});
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    popup.classList.add("show");
+  }, 600);
+});
+
 /*page-wrap carrusel */
 
